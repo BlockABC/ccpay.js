1e5f0bd91f3accb15adad939932f12f30404d53c
jeffjing <zgayjjf@qq.com>
2020-07-08 19:24:37 +0800

    refactor: improve JsonRpc check

--- src/channel/JsonRpc.ts
+++ src/channel/JsonRpc.ts
@@ -26,14 +26,18 @@ export interface JsonRpcResponse {
 
 export type JsonRpcPayload = JsonRpcEvent | JsonRpcRequest | JsonRpcResponse
 
-export function isRpcResponse (rpc: JsonRpcPayload): rpc is JsonRpcResponse {
-  return (rpc as any).id && ((rpc as any).result || (rpc as any).error)
+export function isRpcPayload (rpc: any): boolean {
+  return rpc.jsonrpc === '2.0'
 }
 
-export function isRpcEvent (rpc: JsonRpcPayload): rpc is JsonRpcEvent {
-  return (rpc as any).method && !(rpc as any).id
+export function isRpcResponse (rpc: any): rpc is JsonRpcResponse {
+  return isRpcPayload(rpc) && !rpc.method && rpc.id
 }
 
-export function isRpcRequest (rpc: JsonRpcPayload): rpc is JsonRpcRequest {
-  return (rpc as any).method && (rpc as any).id
+export function isRpcEvent (rpc: any): rpc is JsonRpcEvent {
+  return isRpcPayload(rpc) && rpc.method && !rpc.id
+}
+
+export function isRpcRequest (rpc: any): rpc is JsonRpcRequest {
+  return isRpcPayload(rpc) && rpc.method && rpc.id
 }
